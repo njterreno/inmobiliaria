0300a10987fdc297d90797cdc16539cae0155611
NatanaelTerreno <73486181+njterreno@users.noreply.github.com>
2021-07-19 21:15:00 -0300

    Add files via upload

---
+++ ControlesPersonalizados/cbxBusqueda.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados
+{
+    public partial class cbxBusqueda : ComboBox
+    {
+        public cbxBusqueda()
+        {
+            InitializeComponent();
+        }
+
+        private void cbxBusqueda_KeyDown(object sender, KeyEventArgs e)
+       {
+            if (e.KeyCode != Keys.Back)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                cbxBusqueda cbx = new cbxBusqueda();
+                cbx.Text = "";
+            }
+        }
+
+        private void cbxBusqueda_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != Convert.ToChar( Keys.Back))
+            {
+                e.Handled = true;
+            }
+            
+        }
+
+        
+    }
+}
---
+++ ControlesPersonalizados/cbxComboSinModif.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados
+{
+    public partial class cbxComboSinModif : ComboBox
+    {
+        public cbxComboSinModif()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void cbxComboSinModif_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == Convert.ToChar(Keys.Delete))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void cbxComboSinModif_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
